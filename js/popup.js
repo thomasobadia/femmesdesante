2e5d1c0c40c37f322a9cde33e1db03a164b723c3
thomasobadia <thomas.obaida@hetic.net>
2019-06-16 17:25:06 +0200

    popup style + or

--- js/popup.js
+++ js/popup.js
@@ -1,23 +1,26 @@
 let i = 0;
-let txt = 'Ce site a pour but de mettre en lumière des femmes inspirantes qui ont réalisé des initiatives de santé utiles.'
+let txt = 'Bienvenue sur Femmes de Santé. * Ce site a pour but de mettre en lumière des femmes inspirantes qui ont réalisé des initiatives de santé utiles.'
 let speed = 40; /* The speed/duration of the effect in milliseconds */
 
 const typeWriter = () => {
   if (i < txt.length) {
+    if(txt.charAt(i) != "*"){
     document.getElementById("popup--text").innerHTML += txt.charAt(i);
+
+    }else{
+      document.getElementById("popup--text").innerHTML += '<br>'
+    }
     i++;
     setTimeout(typeWriter, speed);
   }
 //   $('body').css('overflow', 'hidden')
 }
 
-setTimeout(() => {
-  typeWriter()
-}, 1000);
+typeWriter()
 
 setTimeout(() => {
     $('.home__popup__next').text("Suivant")
-}, (txt.length*speed + 1500));
+}, (txt.length*speed + 1000));
 
 $('.home__popup__next').click(function(){
     $('.home__popup').fadeOut()
